099c9fb9514be78be71d4a0ef61ff24ab0343641
saviosantanna <saviosantannadesa@gmail.com>
2022-04-21 19:09:24 -0300

    day 2

--- 0-Newbie/4-interactive-rating-component-main/script.js
+++ 0-Newbie/4-interactive-rating-component-main/script.js
@@ -1,8 +1,18 @@
-function noteActive(x){
-    
-    for (i=1; i<=5; i++){
-        window.document.getElementById(`note${i}`).style.backgroundColor = "hsla(213, 20%, 35%, 0.20)"
-    }    
-    var numNote = `note${x}`
+var Active = true;
+var numActive;
+
+function noteActive(z){
+    var numNote = `note${z}`
+    numActive = z;
+    if (Active == true){
     window.document.getElementById(numNote).style.backgroundColor = "hsl(217, 12%, 63%)"
+    Active = false;
+    } else {
+        for (i=1; i<=5; i++){
+            numNote = `note${i}`
+            window.document.getElementById(numNote).style.backgroundColor = "none"
+        }
+        window.document.getElementById(numNote).style.backgroundColor = "none"
+    }
+    
 }
