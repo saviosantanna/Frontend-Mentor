16b454a64adad2e48435868c4977cff2673a362a
saviosantanna <saviosantannadesa@gmail.com>
2023-02-08 18:14:35 -0300

    finalizacao-javascript

--- 0-Newbie/4-interactive-rating-component-main/script.js
+++ 0-Newbie/4-interactive-rating-component-main/script.js
@@ -1,5 +1,5 @@
 var Active = true;
-var numActive;
+window.document.getElementById('finish').hidden=true;
 
 function noteActive(z){
     var numNote = `note${z}`
@@ -12,5 +12,19 @@ function noteActive(z){
         }
         window.document.getElementById(numNote).style.backgroundColor = "hsl(217, 12%, 63%)"
     }
+
+    var newSpan = document.createElement('span');
+    var includeSpan = document.createTextNode(`You selected ${z} out of 5`);
+    newSpan.appendChild(includeSpan);
+    var newTag = document.getElementById('finish');
+    newTag.insertBefore(newSpan, newTag.children[0]);
 }
+
+function tkstate(){
+    window.document.getElementById('start').hidden=true;
+    window.document.getElementById('finish').hidden=false;
+    window.document.getElementById('my-style').setAttribute('href', 'style2.css');
+    
+}
+
 /*"hsla(213, 20%, 35%, 0.20)"*/
