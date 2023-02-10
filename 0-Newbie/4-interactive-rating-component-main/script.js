3fa984261a8c750fe370defd8bff518ec9b51496
saviosantanna <saviosantannadesa@gmail.com>
2023-02-10 12:16:49 -0300

    finish-challange

--- 0-Newbie/4-interactive-rating-component-main/script.js
+++ 0-Newbie/4-interactive-rating-component-main/script.js
@@ -1,30 +1,50 @@
 var Active = true;
+var Active2;
 window.document.getElementById('finish').hidden=true;
 
 function noteActive(z){
     var numNote = `note${z}`
+    var newSpan = document.createElement('span');
+    var includeSpan = document.createTextNode(`You selected ${z} out of 5`);
+    newSpan.setAttribute('id', 'txt-note');
     if(Active){
         window.document.getElementById(numNote).style.backgroundColor = "hsl(217, 12%, 63%)"
         Active = false;
+        Active2 = true;
     } else {
         for (i=1; i<=5; i++){
             window.document.getElementById(`note${i}`).style.cssText = "hsla(213, 20%, 35%, 0.20)"
         }
         window.document.getElementById(numNote).style.backgroundColor = "hsl(217, 12%, 63%)"
+
+        newSpan.appendChild.createTextNode="doideira"
     }
 
-    var newSpan = document.createElement('span');
-    var includeSpan = document.createTextNode(`You selected ${z} out of 5`);
-    newSpan.appendChild(includeSpan);
-    var newTag = document.getElementById('finish');
-    newTag.insertBefore(newSpan, newTag.children[0]);
+    if(Active2){
+        newSpan.appendChild(includeSpan);
+        var newTag = document.getElementById('finish');
+        newTag.insertBefore(newSpan, newTag.children[1]);
+        Active2=false;
+        window.document.getElementsByTagName('span').setAttribute('id','txt-note');
+    } else {
+        
+        document.getElementById('txt-note').innerHTML = `You selected ${z} out of 5`;
+    }
 }
 
 function tkstate(){
     window.document.getElementById('start').hidden=true;
     window.document.getElementById('finish').hidden=false;
     window.document.getElementById('my-style').setAttribute('href', 'style2.css');
-    
+
 }
 
+
+
 /*"hsla(213, 20%, 35%, 0.20)"*/
+
+    /*var newSpan = document.createElement('span');
+    var includeSpan = document.createTextNode(`You selected ${z} out of 5`);
+    newSpan.appendChild(includeSpan);
+    var newTag = document.getElementById('finish');
+    newTag.insertBefore(newSpan, newTag.children[0]);*/
